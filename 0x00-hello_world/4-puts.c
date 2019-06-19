64744d90e07eec3d02d30108e55bdac2233e9ca2
Daransoto <901@holbertonschool.com>
2019-06-19 17:25:24 +0000

    Added description for function main

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/**
+* main - Entry function, prints a message
+* Return: 0
+*/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
