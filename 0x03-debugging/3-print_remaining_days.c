7414f5d3b9e04cd0a3e9f7336ef4089d036aff38
Daransoto <901@holbertonschool.com>
2019-06-25 22:16:11 +0000

    Modified line length

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,7 +12,7 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0 && !(year % 100 == 0)) || (year % 400 == 0 && year % 100 == 0))
+	if ((year % 4 == 0 && year % 100) || (!(year % 400) && !(year % 100)))
 	{
 		if (month >= 2 && day > 60)
 		{
