47c8a3a82369254451c4eb2eea855b6fd2b1c6d6
Daransoto <901@holbertonschool.com>
2019-06-25 19:12:22 +0000

    Updated last advanced using long double

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,14 +5,14 @@
 */
 int main(void)
 {
-	double n1 = 1, n2 = 2, r;
+	long double n1 = 1, n2 = 2, r;
 	int i;
 
 	printf("1, 2, ");
 	for (i = 0; i < 96; i++)
 	{
 		r = n1 + n2;
-		printf("%.0f", r);
+		printf("%.0Lf", r);
 		n1 = n2;
 		n2 = r;
 		if (i != 95)
