d87b871c89a530515aad09342152e31f526b274e
Daransoto <901@holbertonschool.com>
2019-06-25 21:56:12 +0000

    Changed to unsigned long again

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,14 +5,14 @@
 */
 int main(void)
 {
-	long double n1 = 1, n2 = 2, r;
+	unsigned long int n1 = 1, n2 = 2, r=0;
 	int i;
 
 	printf("1, 2, ");
 	for (i = 0; i < 96; i++)
 	{
 		r = n1 + n2;
-		printf("%.0Lf", r);
+		printf("%lu", r);
 		n1 = n2;
 		n2 = r;
 		if (i != 95)
