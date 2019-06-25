4474de3952f69198b6443d37d4dd0f2bd0dbe6cf
Daransoto <901@holbertonschool.com>
2019-06-25 16:14:47 +0000

    Solution to advanced #4 using long ints and conditionals

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*/
+void main(void)
+{
+	long int n1 = 1, n2 = 2, r, sum = 2;
+
+	while (1)
+	{
+		r = n1 + n2;
+		if (r > 4000000)
+			break;
+		if (r % 2 == 0)
+			sum += r;
+		n1 = n2;
+		n2 = r;
+	}
+	printf("%ld\n", sum);
+}
