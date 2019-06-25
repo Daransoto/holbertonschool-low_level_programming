ceb7cb87def51cd4c75cf1e14f22b76743e4a30d
Daransoto <901@holbertonschool.com>
2019-06-25 14:51:08 +0000

    Created add function for point 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+* add - Adds two numbers.
+* @n1: First number.
+* @n2: Second number.
+* Return: The sum of the two numbers.
+*/
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
