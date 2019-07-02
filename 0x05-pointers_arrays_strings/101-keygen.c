c9b6dfda386e2f631b0e394e45b04634a01de869
Daransoto <901@holbertonschool.com>
2019-07-02 20:38:41 +0000

    Solution to #11

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,31 @@
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* main - Entry point.
+* Return: Always 0.
+*/
+int main(void)
+{
+	int sum = 0, random, i = 0;
+	char pass[10000], *res = pass;
+
+	while (sum < 2772)
+	{
+		srand(time(NULL));
+		random = rand() % 96 + 32;
+		if (2772 - sum > 126)
+		{
+			pass[i] = random;
+			sum += random;
+		}
+		else
+		{
+			pass[i] = 2772 - sum;
+			sum = 2772;
+		}
+		i++;
+	}
+	printf("%s", res);
+	return (0);
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -9,4 +9,4 @@
 7-puts_half.c -> Prints half of a string, followed by a new line.\
 9-strcpy.c -> Copies the string pointed to by src, including the terminating null byte (\0), to the buffer pointed to by dest.\
 100-atoi.c -> Converts a string to an integer.\
-
+101-keygen.c -> Generates random valid passwords for the program 101-crackme.
