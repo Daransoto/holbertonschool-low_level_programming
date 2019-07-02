b4ce65960ad2afb2d990fc7fb0eae7ba9b1c1d5a
Daransoto <901@holbertonschool.com>
2019-07-02 20:50:33 +0000

    Corrected special case where difference was less that 32, and random generation

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,14 +10,19 @@ int main(void)
 	int sum = 0, random, i = 0;
 	char pass[10000], *res = pass;
 
+	srand(time(NULL));
 	while (sum < 2772)
 	{
-		srand(time(NULL));
 		random = rand() % 96 + 32;
 		if (2772 - sum > 126)
 		{
 			pass[i] = random;
 			sum += random;
+		}
+		else if (2772 - sum < 32)
+		{
+			i = 0;
+			sum = 0;
 		}
 		else
 		{
