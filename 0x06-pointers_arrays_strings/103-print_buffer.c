4f06b1b602118578374448877dffac659af0c5a1
Daransoto <901@holbertonschool.com>
2019-07-05 00:40:28 +0000

    Save #2

--- 0x06-pointers_arrays_strings/103-print_buffer.c
+++ 0x06-pointers_arrays_strings/103-print_buffer.c
@@ -7,19 +7,22 @@
 void print_buffer(char *b, int size)
 {
 	int addr = 0, i = 0;
-	char *c = b;
+	char *c = b, e[1000], *d = e;
 
 	while (i++ < size)
+	{
+		d[i - 1] = c[i - 1];
 		if (c[i - 1] < 32 || c[i - 1] > 126)
 			c[i - 1] = '.';
+	}
 	for (i = 0; i < size / 10; i++)
 	{
 		printf("%08x: ", addr);
-		printf("%02x%02x ", *b, *(b + 1));
-		printf("%02x%02x ", *(b + 2), *(b + 3));
-		printf("%02x%02x ", *(b + 4), *(b + 5));
-		printf("%02x%02x ", *(b + 6), *(b + 7));
-		printf("%02x%02x ", *(b + 8), *(b + 9));
+		printf("%02x%02x ", *d, *(d + 1));
+		printf("%02x%02x ", *(d + 2), *(d + 3));
+		printf("%02x%02x ", *(d + 4), *(d + 5));
+		printf("%02x%02x ", *(d + 6), *(d + 7));
+		printf("%02x%02x ", *(d + 8), *(d + 9));
 		printf("%c%c", *b, *(b + 1));
 		printf("%c%c", *(b + 2), *(b + 3));
 		printf("%c%c", *(b + 4), *(b + 5));
@@ -27,12 +30,14 @@ void print_buffer(char *b, int size)
 		printf("%c%c\n", *(b + 8), *(b + 9));
 		addr += 10;
 		b += 10;
+		d += 10;
 	}
 	printf("%08x: ", addr);
-	for (i = 0; i < size % 10 - 1; i++)
+	for (i = 0; i < size % 10 / 2; i++)
 	{
-		printf("%02x%02x ", *(b + i), *(b + i + 1));
-		b++;
+		printf("%02x%02x ", *(d + i), *(d + i + 1));
+		d++;
 	}
-	printf("%02x%02x\n",*(b + i), *(b + i + 1));
+	for (i = 0; i < (10 - size % 10) / 2; i++)
+		printf("     ");
 }
