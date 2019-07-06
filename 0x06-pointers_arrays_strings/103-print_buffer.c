7fe6410d189cbce9637db29dd23b1cfc7decfc51
Daransoto <901@holbertonschool.com>
2019-07-06 03:34:30 +0000

    Changed number of spaces generated

--- 0x06-pointers_arrays_strings/103-print_buffer.c
+++ 0x06-pointers_arrays_strings/103-print_buffer.c
@@ -39,8 +39,8 @@ void print_buffer(char *b, int size)
 			if (i % 2)
 				putchar(' ');
 		}
-		for (i = 0; i < size % 10; i++)
-			printf("      ");
+		for (i = 0; i < (size - 1) % 10; i++)
+			printf("         ");
 		for (i = 0; i < size % 10; i++)
 			printf("%c", *b++);
 	}
