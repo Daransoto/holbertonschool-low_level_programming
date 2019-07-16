7be113bebc26b342e352cf1451980fea141490ef
Daransoto <901@holbertonschool.com>
2019-07-16 22:24:03 +0000

    Added solution to advanced task

---
+++ 0x0B-malloc_free/100-strtow.c
@@ -0,0 +1,87 @@
+#include <stdlib.h>
+/**
+* count_words - Count the number of words on a given string.
+* @str: string to count words.
+* Return: Amount of words.
+*/
+int count_words(char *str)
+{
+	int wc = 0, word = 0;
+
+	for (; *str; str++)
+		if (*str != ' ' && !word)
+		{
+			wc++;
+			word = 1;
+		}
+		else if (*str == ' ')
+		{
+			word = 0;
+		}
+	return (wc);
+}
+/**
+* alloc_array - Creates array with words size.
+* @tmp: String to evaluate.
+* @arr: Array defined to store the result.
+* Return: Array ready to be filled.
+*/
+char **alloc_array(char *tmp, char **arr)
+{
+	int wc = 0, word = 0, i = 0;
+
+	for (; *tmp; tmp++)
+		if (*tmp != ' ')
+		{
+			wc++;
+			word = 1;
+		}
+		else if (*tmp == ' ' && word)
+		{
+			word = 0;
+			arr[i] = malloc(wc + 1);
+			wc = 0;
+			if (!arr[i])
+			{
+				while (i)
+				{
+					free(arr[i]);
+					i--;
+				}
+				free(arr);
+				return (0);
+			}
+			i++;
+		}
+	return (arr);
+}
+/**
+* strtow - Splits a string into words.
+* @str: String to split.
+* Return: Vector of words, or NULL if failed..
+*/
+char **strtow(char *str)
+{
+	int wc = 0, word = 0, i = 0, j = 0;
+	char **arr;
+
+	wc = count_words(str);
+	arr = malloc(wc * sizeof(char *));
+	if (!arr)
+		return (0);
+
+	arr = alloc_array(str, arr);
+	for (; *str; str++)
+		if (*str != ' ')
+		{
+			arr[i][j++] = *str;
+			word = 1;
+		}
+		else if (*str == ' ' && word)
+		{
+			word = 0;
+			arr[i++][j] = '\0';
+			j = 0;
+		}
+	return (arr);
+}
