b1e8c9475c2e2fce0ae964517c181683542c12f4
Daransoto <901@holbertonschool.com>
2019-07-18 19:12:54 +0000

    Added case of < 0

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,7 +10,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *memory;
 	unsigned int i = 0;
 
-	if (!nmemb || !size)
+	if (nmemb <= 0 || size <= 0)
 		return (0);
 
 	memory = malloc(nmemb * size);
