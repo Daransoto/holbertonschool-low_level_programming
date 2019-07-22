6fc56bf2b6a2bfec4c66de2be8faecfab5b8864a
Daransoto <901@holbertonschool.com>
2019-07-22 17:24:09 +0000

    Fixed case of age 0

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,7 +15,7 @@ void print_dog(struct dog *d)
 			d->owner = "(nil)";
 
 		printf("Name: %s\n", d->name);
-		if (!(d->age))
+		if (d->age <= 0)
 			printf("Age: (nil)\n");
 		else
 			printf("Age: %f\n", d->age);
