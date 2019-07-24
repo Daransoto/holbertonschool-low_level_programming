1c70c3ff0caec9be5ddf1394df6ff59feda686a4
Daransoto <901@holbertonschool.com>
2019-07-24 17:27:16 +0000

    Added check for parameters not being NULL

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -9,8 +9,9 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	for (i = 0; i < size; i++)
-		if (cmp(array[i]))
-			return (i);
+	if (array && size && cmp)
+		for (i = 0; i < size; i++)
+			if (cmp(array[i]))
+				return (i);
 	return (-1);
 }
