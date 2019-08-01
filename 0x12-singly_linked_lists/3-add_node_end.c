11cc413646af040685d2229cb04f0088940cd862
Daransoto <901@holbertonschool.com>
2019-08-01 18:37:40 +0000

    Fixed Betty style

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -28,7 +28,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *new, *h = *head;
 
 	if (!head)
-		return(NULL);
+		return (NULL);
 	new = malloc(sizeof(list_t));
 	if (!new)
 		return (0);
