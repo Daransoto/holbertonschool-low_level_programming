74563109a9be6fbf48c82a4a10c53768b28168b7
Daransoto <901@holbertonschool.com>
2019-08-01 17:54:11 +0000

    Removed extra space

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,5 +2,5 @@
 void myConstructor(void) __attribute__ ((constructor));
 void myConstructor(void)
 {
-	printf ("You're beat! and yet, you must allow,\nI bore my house upon my back!\n ");
+	printf ("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
