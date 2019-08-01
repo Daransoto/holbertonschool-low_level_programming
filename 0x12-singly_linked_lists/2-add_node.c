f234bda6c707b1761fb9d336fb67be7f83edca0a
Daransoto <901@holbertonschool.com>
2019-08-01 15:08:53 +0000

    Function that adds a node at the beggining of a linked list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+/**
+* _strlen - Gives the length of a string.
+* @s: String to evaluate.
+* Return: The length of the given string.
+*/
+int _strlen(char *s)
+{
+	short n = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		n++;
+	}
+	return (n);
+}
+/**
+* add_node - Adds a node at the beggining of a list.
+* @head: Pointer to the current first element of the list.
+* @str: String to initialize the new element.
+* Return: Pointer to the new first element or 0 if it fails.
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new = malloc(sizeof(list_t));
+
+	if (!new)
+		return (0);
+	new->str = strdup(str);
+	new->len = _strlen(new->str);
+	new->next = *head;
+	*head = new;
+	return (*head);
+}
