40faf95240c9a147da03504d388e3de934cce409
Daransoto <901@holbertonschool.com>
2019-08-06 16:31:47 +0000

    Added case where head is NULL

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,7 +11,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *iterator = *head, *new;
 
-	if (!*head)
+	if (!*head || !head)
 		return (0);
 
 	while (idx > 1)
