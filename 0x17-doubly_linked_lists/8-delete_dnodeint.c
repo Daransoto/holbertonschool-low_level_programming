9292a8e80e095106f0b29f10253bbc827e395d58
Daransoto <901@holbertonschool.com>
2019-09-14 18:10:13 +0000

    Fixed case when there's no next node when removing

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -29,7 +29,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (!temp)
 		return (-1);
 	iterator->next = temp->next;
-	temp->next->prev = iterator;
+	if (temp->next)
+		temp->next->prev = iterator;
 	free(temp);
 	return (1);
 }
