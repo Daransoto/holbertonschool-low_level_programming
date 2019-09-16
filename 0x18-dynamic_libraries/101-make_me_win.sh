1bf4365a8848908d5cdad609d890694da8b4c7cf
Daransoto <901@holbertonschool.com>
2019-09-16 18:59:38 +0000

    Fixed use of current dir

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://github.com/Daransoto/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/inject.so
-export LD_PRELOAD=$PWD/inject.so
+wget https://github.com/Daransoto/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/inject.so -P ../
+export LD_PRELOAD=$PWD/../inject.so
