064ec8b5b080593a473087fd0677ea5f74e27909
Daransoto <901@holbertonschool.com>
2019-11-15 21:30:19 +0000

    Checking edges

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,27 +4,25 @@
 
 def island_perimeter(grid):
     """Function that calculates the perimeter of an island on a grid."""
-    i = 1
-    j = 1
+    i = 0
+    j = 0
     perimeter = 0
-    if grid is None or type(grid) is not list:
+    if grid is None or type(grid) is not list or type(grid[0]) is not list:
         return 0
     length = len(grid)
     length2 = len(grid[0])
-    if 1 in grid[0] or 1 in grid[length - 1]:
-        return 0
-    while i < length - 1:
-        while j < length2 - 1:
+    while i < length:
+        while j < length2:
             if grid[i][j] == 1:
-                if grid[i - 1][j] == 0:
+                if i == 0 or grid[i - 1][j] == 0:
                     perimeter += 1
-                if grid[i][j - 1] == 0:
+                if j == 0 or grid[i][j - 1] == 0:
                     perimeter += 1
-                if grid[i][j + 1] == 0:
+                if j == length2 - 1 or grid[i][j + 1] == 0:
                     perimeter += 1
-                if grid[i + 1][j] == 0:
+                if i == length - 1 or grid[i + 1][j] == 0:
                     perimeter += 1
             j += 1
-        j = 1
+        j = 0
         i += 1
     return perimeter
