3e8e11dc5aa3e94dfbb51de5ed032519d2556fda
Daransoto <901@holbertonschool.com>
2019-11-15 19:44:32 +0000

    Solution to task 5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""island_ perimeter module."""
+
+def island_perimeter(grid):
+    """Function that calculates the perimeter of an island on a grid."""
+    i = 1
+    j = 1
+    perimeter = 0
+    length = len(grid)
+    length2 = len(grid[0])
+    while i < length - 1:
+        while j < length2 - 1:
+            if grid[i][j] == 1:
+                if grid[i - 1][j] == 0:
+                    perimeter += 1
+                if grid[i][j - 1] == 0:
+                    perimeter += 1
+                if grid[i][j + 1] == 0:
+                    perimeter += 1
+                if grid[i + 1][j] == 0:
+                    perimeter += 1
+            j += 1
+        j = 1
+        i += 1
+    return perimeter
