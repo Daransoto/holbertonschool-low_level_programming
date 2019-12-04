0a050c785d42d681da0d4de64b9b2868a07e52ad
Daransoto <901@holbertonschool.com>
2019-12-04 19:26:24 +0000

    Fixed Betty style

--- 0x1D-binary_trees/13-binary_tree_nodes.c
+++ 0x1D-binary_trees/13-binary_tree_nodes.c
@@ -9,7 +9,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 	if (tree->left || tree->right)
-		return ( 1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+		return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
 	else
 		return (0);
 }
