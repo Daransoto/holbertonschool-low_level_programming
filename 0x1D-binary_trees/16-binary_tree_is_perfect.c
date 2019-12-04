248d6cf11d13b49f1397e3eb8c17a5b03cf7a2f3
Daransoto <901@holbertonschool.com>
2019-12-04 20:00:17 +0000

    Checking balance on each node

--- 0x1D-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1D-binary_trees/16-binary_tree_is_perfect.c
@@ -65,6 +65,12 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 	if (binary_tree_is_full(tree) && !binary_tree_balance(tree))
-		return (1);
+	{
+		if (tree->left)
+			return (1 * binary_tree_is_perfect(tree->left) *
+binary_tree_is_perfect(tree->right));
+		else
+			return (1);
+	}
 	return (0);
 }
