32daa0a527e1f50e9440094beb2f62c306580436
Daransoto <901@holbertonschool.com>
2019-12-05 22:54:06 +0000

    Fixed Betty style

--- 0x1D-binary_trees/114-bst_remove.c
+++ 0x1D-binary_trees/114-bst_remove.c
@@ -21,19 +21,44 @@ bst_t *bst_search(const bst_t *tree, int value)
 	return (NULL);
 }
 /**
+* bst_remove_2 - Removes a node from a Binary Search Tree.
+* @root: Pointer to the root node of the tree where you will remove a node.
+* @node: Node with value to remove in the tree.
+* Return: Pointer to the new root node of the tree after removing the value.
+*/
+bst_t *bst_remove_2(bst_t *root, bst_t *node)
+{
+	if (node->left)
+	{
+		if (node == root)
+			root = node->left;
+		else
+			if (node->parent->left == node)
+				node->parent->left = node->left;
+			else
+				node->parent->right = node->left;
+		node->left->parent = node->parent;
+	}
+	else
+	{
+		if (node == root)
+			root = node->right;
+		node->right->parent = node->parent;
+	}
+	return (root);
+}
+/**
 * bst_remove - Removes a node from a Binary Search Tree.
 * @root: Pointer to the root node of the tree where you will remove a node.
 * @value: Value to remove in the tree.
-* Return: Pointer to the new root node of the tree after removing the desired value.
+* Return: Pointer to the new root node of the tree after removing the value.
 */
 bst_t *bst_remove(bst_t *root, int value)
 {
 	bst_t *node, *iterator;
 
-	if (!root)
-		return (NULL);
 	node = bst_search(root, value);
-	if (!node)
+	if (!root || !node)
 		return (NULL);
 	if (!node->left && !node->right)
 	{
@@ -43,7 +68,6 @@ bst_t *bst_remove(bst_t *root, int value)
 			node->parent->left = NULL;
 		else
 			node->parent->right = NULL;
-		free(node);
 	}
 	else if (node->left && node->right)
 	{
@@ -57,32 +81,12 @@ bst_t *bst_remove(bst_t *root, int value)
 				node->parent->left = iterator;
 			else
 				node->parent->right = iterator;
-		iterator->left = node->left;
-		iterator->right = node->right;
-		node->left->parent = iterator;
-		node->right->parent = iterator;
-		iterator->parent->left = NULL;
-		iterator->parent = node->parent;
-		free(node);
-	}
-	else if (node->left)
-	{
-		if (node == root)
-			root = node->left;
-		else
-			if (node->parent->left == node)
-				node->parent->left = node->left;
-			else
-				node->parent->right = node->left;
-		node->left->parent = node->parent;
-		free(node);
+		iterator->left = node->left, iterator->right = node->right;
+		node->left->parent = iterator, node->right->parent = iterator;
+		iterator->parent->left = NULL, iterator->parent = node->parent;
 	}
 	else
-	{
-		if (node == root)
-			root = node->right;
-		node->right->parent = node->parent;
-		free(node);
-	}
+		root = bst_remove_2(root, node);
+	free(node);
 	return (root);
 }
