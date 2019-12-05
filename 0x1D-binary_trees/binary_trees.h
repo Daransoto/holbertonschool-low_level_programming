df682b57bea751df4e2b855ee30071bcb42501f9
Daransoto <901@holbertonschool.com>
2019-12-05 15:55:27 +0000

    updated header file

--- 0x1D-binary_trees/binary_trees.h
+++ 0x1D-binary_trees/binary_trees.h
@@ -45,4 +45,5 @@ const binary_tree_t *second);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 void traverse_level(const binary_tree_t *tree, void (*func)(int),
  size_t level);
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 #endif /* _BINARY_TREES_H_ */
