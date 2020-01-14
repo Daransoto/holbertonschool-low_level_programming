8c683702b21acd18f0ed9493a3119c32601cbcab
Daransoto <901@holbertonschool.com>
2020-01-14 21:51:30 +0000

    First upload of solutions to tasks o to advanced task 1

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,9 @@
+#ifndef _SEARCH_ALGS_H_
+#define _SEARCH_ALGS_H_
+
+#include <stdio.h>
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+
+#endif /* _SEARCH_ALGS_H_ */
